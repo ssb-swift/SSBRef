bcf1fe9828d7278b5698d2f6e922666a5d94df69
David Gomez <dg.dave@hey.com>
2021-06-05 12:50:12 -0400

    Format Package.swift file using swift-format

--- Package.swift
+++ Package.swift
@@ -4,28 +4,30 @@
 import PackageDescription
 
 let package = Package(
-    name: "SSBRef",
-    platforms: [
-        .macOS(.v10_15),
-        .iOS(.v13),
-        .watchOS(.v6),
-        .tvOS(.v13)
-    ],
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "SSBRef", targets: ["SSBRef"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/sindresorhus/Regex", from: "0.1.1")
-    ],
-    targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "SSBRef", dependencies: [
-            .product(name: "Regex", package: "Regex")
-        ]),
-        .testTarget(name: "SSBRefTests", dependencies: ["SSBRef"]),
-    ]
+  name: "SSBRef",
+  platforms: [
+    .macOS(.v10_15),
+    .iOS(.v13),
+    .watchOS(.v6),
+    .tvOS(.v13),
+  ],
+  products: [
+    // Products define the executables and libraries a package produces, and make them visible to other packages.
+    .library(name: "SSBRef", targets: ["SSBRef"])
+  ],
+  dependencies: [
+    // Dependencies declare other packages that this package depends on.
+    // .package(url: /* package url */, from: "1.0.0"),
+    .package(url: "https://github.com/sindresorhus/Regex", from: "0.1.1")
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+    // Targets can depend on other targets in this package, and on products in packages this package depends on.
+    .target(
+      name: "SSBRef",
+      dependencies: [
+        .product(name: "Regex", package: "Regex")
+      ]),
+    .testTarget(name: "SSBRefTests", dependencies: ["SSBRef"]),
+  ]
 )
